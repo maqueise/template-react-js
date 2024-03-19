23a8128ea9423f4e94d62623aab2fed3c44bbfac
maqueise <caio.pinheiro@ifms.edu.br>
2024-03-19 17:35:34 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Sparkles } from "lucide-react"
+
+function App() {
+  
+
+  return (
+      <div className="flex flex-col items-center gap-4 mt-5">
+        <h1 className="text-3xl text-blue-600">Template REACT JS</h1>
+        <h2 className="text-2xl">Desenvolvimento de Interfaces Ricas</h2>
+        <Sparkles className="text-amber-500" size={32}/>
+      </div>
+  )
+}
+
+export default App
