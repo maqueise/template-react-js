1048e94289d45daa338c71ca1a458438607f7051
Caio Maqueise Alécio Pinheiro <caio.pinheiro@ifms.edu.br>
2024-03-20 02:20:17 +0000

    upadate jsx app

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ function App() {
   
 
   return (
-      <div className="flex flex-col items-center gap-4 mt-5">
+      <section className="container mx-auto flex flex-col items-center gap-4 mt-5">
         <h1 className="text-3xl text-blue-600">Template REACT JS</h1>
         <h2 className="text-2xl">Desenvolvimento de Interfaces Ricas</h2>
         <Sparkles className="text-amber-500" size={32}/>
-      </div>
+      </section>
   )
 }
 
